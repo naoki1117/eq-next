f12d24a7e5755fa143dee45fce2950fde2bc2d2d
Your Name <you@example.com>
2023-03-26 21:09:02 +0900

    yrykkkv

--- components/EqList.tsx
+++ components/EqList.tsx
@@ -8,7 +8,7 @@ export const EqList = () => {
     const {data: eqs, status} =useQueryEqs()
     if (status === "loading") return <Loader my="lg" color="cyan"/>
   return (
-    <div className='basis-2/4'>
+    <div className='basis-1/2'>
         <List
             my="lg"
             spacing="sm"
--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -29,7 +29,7 @@ const Dashboard:NextPage = () => {
         />
         <UserInfo/>
         <Header/>
-        <div className='flex h-full'>
+        <div className='flex flex-row h-full'>
           <EqForm/>
           <EqList/>
           <Link/>
