518744c217b3857444eb9a0a137bfac7175f535f
Your Name <you@example.com>
2023-03-26 22:40:19 +0900

    hgkihl;l

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,7 +8,7 @@ export const Header = () => {
         <Link href="/information">
             <InformationCircleIcon className='h-5 w-5 cursor-pointer'/>
         </Link>
-        <p className="">備品管理システム</p>
+        <p className="m-0">備品管理システム</p>
         <div className='h-[20px] bg-indigo-500'></div>
     </>
   )
--- components/Link.tsx
+++ components/Link.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Link = () => {
   return (
     <div className="col-span-1 border-slate-400">
-      <a href="https://realtime-remind.vercel.app/">remindapp</a>
+      <a href="https://realtime-remind.vercel.app/" className='no-underline text-gray-500'>remindapp</a>
     </div>
   )
 }
