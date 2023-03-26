1ca587aa70dd52b05e7d0a951c8c25ee62eb2c06
Your Name <you@example.com>
2023-03-26 22:47:25 +0900

    et6hrjyutkyi

--- components/Link.tsx
+++ components/Link.tsx
@@ -1,8 +1,10 @@
+import { Title } from '@mantine/core'
 import React from 'react'
 
 export const Link = () => {
   return (
-    <div className="col-span-1 border-slate-400">
+    <div className="col-span-1 border-1-double">
+        <Title order={3} align="center">外部リンク</Title>
       <a href="https://realtime-remind.vercel.app/" className='no-underline text-gray-500'>remindapp</a>
     </div>
   )
